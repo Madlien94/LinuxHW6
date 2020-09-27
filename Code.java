153aa3cba65e3b0b40eb460fee6c1e9f238f44c4
Madlien Shehata <Mshehata@njcu.edu>
2020-09-27 22:12:26 +0000

    I am taking CS407 because it's a requirement/ new skill

--- Code.java
+++ Code.java
@@ -1,5 +1,5 @@
 public class Code{
 	public static void main(String[] args){
-		System.out.println("My favorite programming language is java");
+		System.out.println("I am taking CS407 because it's a requirement/ new skill");
 	}
 }
