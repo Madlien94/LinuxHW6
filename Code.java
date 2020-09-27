b90cacd9dafe98aaf70777e58cdf1ad6e35af8a2
Madlien Shehata <Mshehata@njcu.edu>
2020-09-27 22:17:09 +0000

    My favorite food is Pasta and Pizza

--- Code.java
+++ Code.java
@@ -1,5 +1,5 @@
 public class Code{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because it's a requirement/ new skill");
+		System.out.println("My Favorite food is Pasta and Pizza");
 	}
 }
